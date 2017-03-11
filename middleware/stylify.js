0346109c627ac011b80e0544e1c2864a36e70a7e
michael <4apaev@gmail.com>
2017-03-11 20:41:56 +0200

    stylus render

--- middleware/stylify.js
+++ middleware/stylify.js
@@ -1,31 +1,37 @@
 'use strict';
 
 const Fs = require('fs');
+const Path = require('path');
 const Styl = require('stylus');
+const log = console.log.bind(console)
 
+module.exports = stylify;
 
-module.exports = pugify;
+function stylify({ basedir=process.cwd() }={}) {
+  return (req, res, next) => {
+    let filename = Path.join(basedir, req.pathname.replace(/\.css$/, '.styl'))
 
-function pugify(name, locals={}) {
-  return function(req, res, next) {
+    Fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        return res.end()
+      }
 
-      Styl(str)
-        .set('filename', __dirname + '/test.styl')
-        .import('mixins/vendor')
+      Styl(data.toString())
+        .set('filename', filename)
         .render((err, css) => {
-
           if (err)
             return next(err)
 
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/css');
-          res.setHeader('Content-Length', css.length);
+          res.writeHead(200, {
+            'Content-Type': 'text/css',
+            'Content-Length': css.length
+          })
+
           res.end(css);
+        })
 
-        });
-
-
-
+    })
   }
 }
 
